07c09b4859411c808cd64e9c21299622a0763e47
dttrue <danieltorres@pursuit.org>
2023-12-07 10:16:34 -0500

    Daniels CustomMessage component

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,12 @@
+
+import CustomMessage from './CustomMessage';
+
+function App() {
+  return (
+    <div className="App">
+     <CustomMessage message = "Hello World from JSX!"/>
+    </div>
+  );
+}
+
+export default App;
