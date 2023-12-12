8313916c1839a1e4886cf4634c4e3fdfa738b403
EdgarCollado <edgarcollado@pursuit.org>
2023-12-11 20:00:21 -0500

    added-custom-message-component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomMessage from './CustomMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomMessage message='12/11/2023'/>
+    </div>
+  );
+}
+
+export default App;
