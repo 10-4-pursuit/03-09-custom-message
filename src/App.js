15df3e34099cc2a0b620ef7720fdc2501e4949dd
MoniqueC10-4 <monique.charles13@gmail.com>
2023-12-13 22:34:38 -0500

    Add CustomMessage component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import CustomMessage from './CustomMessage.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomMessage
+      message = "Winter is coming"
+      />;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CustomMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CustomMessage(props) {
+    return (
+        <div>
+            {props.message}
+        </div>
+    );
+}
+
+
+export default CustomMessage;
