96dab307be9f8eea688b6054aa64f4ad2b4971bf
clairep9 <clairepetitfrere@pursuit.org>
2023-12-20 21:12:16 -0500

    Add customMessage component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomMessage from './CustomMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomMessage message="I heart coding." />
+    </div>
+  );
+}
+
+export default App;
