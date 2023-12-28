31c1c3cdc60161b1f0d09df1b91ca12b466452ca
SaraM09 <sayeramusfika@pursuit.org>
2023-12-27 22:10:54 -0500

    Add CustomMessage component

---
+++ 03-09-exercise/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import CustomMessage from './CustomMessage';
+
+function App() {
+  return (
+    <div className="App">
+     <CustomMessage message="Dannyisazipfile"/>
+     <CustomMessage message="Dannyisawino"/>
+    </div>
+  );
+}
+
+export default App;
