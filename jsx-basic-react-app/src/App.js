6be889e638534081046e91eb2a20d80312030b81
Azmain Shoeb <azmainshoeb@pursuit.org>
2023-12-28 19:00:26 -0500

    Add CustomMessage component

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomMessage from './CustomMessage';
+
+function App() {
+  return (
+    <div className="App">
+     <CustomMessage message="Hello World!"/>
+    </div>
+  );
+}
+
+export default App;
