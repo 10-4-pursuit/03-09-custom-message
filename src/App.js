4125cff911865fd473b52f4e2adb13f8a816d968
jadequintero <jadequintero@pursuit.org>
2024-01-02 19:27:49 -0500

    Add CustomMessage component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import CustomMessage from './CustomMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomMessage message="This is a custom message!" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CustomMessage.js
@@ -0,0 +1,12 @@
+// CustomMessage.js
+import React from 'react';
+
+const CustomMessage = ({ message }) => {
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default CustomMessage;
