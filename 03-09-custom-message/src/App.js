d5117a23304cdc0daf1ec5ebaf77c64146f39532
lanell1 <lanelllopez@pursuit.org>
2024-01-02 19:05:04 -0500

    complete-03-09-custom-message

---
+++ 03-09-custom-message/src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import CustomMessage from './CustomMessage';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Other components or content in App.js */}
+      
+      {/* Render CustomMessage with a custom message */}
+      <CustomMessage message="Hello, My name is Lanell and I am demonstrating how to display a custom message!" />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ 03-09-custom-message/src/CustomMessage.js
@@ -0,0 +1,13 @@
+// CustomMessage.js
+import React from 'react';
+
+const CustomMessage = ({ message }) => {
+  // Component logic goes here (if needed)
+  return (
+    <div>
+      {message}
+    </div>
+  );
+};
+
+export default CustomMessage;
