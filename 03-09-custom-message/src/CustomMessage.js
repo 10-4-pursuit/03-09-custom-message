f5cc3f6b1fe789dbb836b24675108617cb9003fb
Mikal Din <Mikal Wazeerud-din@pursuit.org>
2024-01-26 09:14:24 -0500

    with much technical difficultites absolved.  it finally happened.

---
+++ 03-09-custom-message/src/CustomMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomMessage = (props) => {
+    return (
+        <div>
+            <p>{props.message}</p>
+        </div>
+
+    );
+};
+
+export default CustomMessage;
